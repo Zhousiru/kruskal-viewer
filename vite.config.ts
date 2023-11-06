701c4f85274429a2ed80ad0f3db9147f86e9dd71
Zhousiru <i@zhousiru.com>
2023-11-06 14:24:02 +0800

    feat: wrapped with tauri

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   base: './',
   plugins: [react()],
 })
